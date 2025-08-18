ed47e208ac06ec70b61bf006ae7d3f2a17aedfbf
Saidolimxon <saidolimxonsoibjonov@gmail.com>
2025-08-18 21:17:53 +0500

    Fix ChevronRight import error

--- src/components/Home/FeaturedProducts.tsx
+++ src/components/Home/FeaturedProducts.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ChevronRight } from 'lucide-react';
 import { getProducts } from '../../data/mockData';
 import ProductGrid from '../Product/ProductGrid';
 
